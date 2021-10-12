3add5f0232dd2934113f279030e00b09beba074c
saadkhawar289 <70708945+saadkhawar289@users.noreply.github.com>
2021-10-12 10:58:36 +0500

    barcode genrator added slectedProduct Screen updated with fetch single product

--- healthy/lib/qr.dart
+++ healthy/lib/qr.dart
@@ -233,6 +233,8 @@
 // }
 
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
@@ -246,26 +248,47 @@ class QRViewExample extends StatefulWidget {
 class _QRState extends State<QRViewExample> {
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           '#ff6666', 'Cancel', true, ScanMode.BARCODE);
       print('=====================$barcodeScanRes');
       if (barcodeScanRes.toString().isNotEmpty) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) =>
-                    SelectedProduct(barcodeScanRes.toString())));
+        showDialog(
+            context: context,
+            builder:
+                (BuildContext context) {
+              return AlertDialog(
+                content: Container(
+                  height: 250,
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Image.asset("assets/ic_logo.png")
+                        ),
+                      Center(
+                        child: Text('Loading item...'),
+                      )
+                    ],
+                  ),
+                ),
+
+              );
+            });
+        Timer(Duration(seconds: 3),(){
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      SelectedProduct(barcodeScanRes.toString())));
+        });
+
       } else {}
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
     } catch (e) {
       print('==================$e');
     }
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
+
     if (!mounted) return;
 
     // setState(() {
