16a9035fcaf526d12973be0f5a792b8ade979cac
saadkhawar289 <70708945+saadkhawar289@users.noreply.github.com>
2021-10-14 16:44:04 +0500

    barcode genrator added slectedProduct Screen updated with fetch single product

--- healthy/lib/Provider/cart_view_model.dart
+++ healthy/lib/Provider/cart_view_model.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:healthy/Model/Product.dart';
-import 'package:provider/provider.dart';
 
+class CartViewModel extends ChangeNotifier {
+  List<Product> cartItems = [];
+  List<Product> addedCartItems = [];
 
-class CartViewModel extends ChangeNotifier{
-
-
-  List<Product>? cartItems;
-
-
-
-  void addToCart(Product product){
-
-    cartItems!.add(product);
-notifyListeners();
+  void addToCart(Product product) {
+    cartItems.add(product);
+    addedCartItems = cartItems;
+    notifyListeners();
   }
-
-
 }
